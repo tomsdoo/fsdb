848a32c93de651bf867f9d15b0888984f239a837
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-09-30 18:25:57 +0900

    prettier is added (#4)
    * prettier is installed
    
    * npm script format:src is added
    
    * format:src
    
    * npm script format:test is added
    
    * format:test
    
    * npm script format is added

--- src/filedb.ts
+++ src/filedb.ts
@@ -3,53 +3,45 @@ import { v4 as uuid } from "uuid";
 
 export class FileDb {
   protected filePath: string;
-  constructor(filePath: string){
+  constructor(filePath: string) {
     this.filePath = filePath;
   }
-  protected async getContent(){
-    return await readFile(
-      this.filePath,
-      { encoding: "utf8" }
-    )
-      .then(text => JSON.parse(text))
-      .catch(e => ({}));
+  protected async getContent() {
+    return await readFile(this.filePath, { encoding: "utf8" })
+      .then((text) => JSON.parse(text))
+      .catch((e) => ({}));
   }
-  protected async saveContent(obj: object){
-    return await writeFile(
-      this.filePath,
-      JSON.stringify(obj),
-      { encoding: "utf8" }
-    );
+  protected async saveContent(obj: object) {
+    return await writeFile(this.filePath, JSON.stringify(obj), {
+      encoding: "utf8",
+    });
   }
-  public async get(id?: string){
-    return await this.getContent()
-      .then(obj => id
-        ? obj[id]
-        : obj
-      );
+  public async get(id?: string) {
+    return await this.getContent().then((obj) => (id ? obj[id] : obj));
   }
-  public async save(obj: object){
+  public async save(obj: object) {
     const objToBeSaved = {
       _id: uuid(),
-      ...obj
+      ...obj,
     };
     return await this.getContent()
-      .then(obj => this.saveContent({
-        ...obj,
-        [objToBeSaved._id]: objToBeSaved
-      }))
+      .then((obj) =>
+        this.saveContent({
+          ...obj,
+          [objToBeSaved._id]: objToBeSaved,
+        })
+      )
       .then(() => objToBeSaved);
   }
-  public async remove(id: string){
+  public async remove(id: string) {
     return await this.getContent()
       .then(({ [id]: any, ...rest }) => this.saveContent(rest))
       .then(() => undefined);
   }
-  public async getIds(){
-    return await this.getContent()
-      .then(obj => Object.keys(obj));
+  public async getIds() {
+    return await this.getContent().then((obj) => Object.keys(obj));
   }
-  public async drop(){
+  public async drop() {
     return await unlink(this.filePath).catch(() => undefined);
   }
 }
--- test/test.ts
+++ test/test.ts
@@ -20,25 +20,22 @@ describe("FileDb", () => {
 
   it("value after initializing is empty object", async () => {
     assert.equal(
-      await fileDb.get()
-        .then(obj => JSON.stringify(obj)),
+      await fileDb.get().then((obj) => JSON.stringify(obj)),
       JSON.stringify({})
     );
   });
 
   it("value of item that does not exist is undefined", async () => {
-    assert.equal(
-      await fileDb.get(uuid()),
-      undefined
-    );
+    assert.equal(await fileDb.get(uuid()), undefined);
   });
 
   it("save() new item", async () => {
     const savingObj = {
-      testValue: uuid()
+      testValue: uuid(),
     };
     assert.equal(
-      await fileDb.save(savingObj)
+      await fileDb
+        .save(savingObj)
         .then(({ _id, ...rest }) => JSON.stringify(rest)),
       JSON.stringify(savingObj)
     );
@@ -46,27 +43,23 @@ describe("FileDb", () => {
 
   it("save() update item", async () => {
     const savingObj = {
-      testValue: "value before updating"
+      testValue: "value before updating",
     };
 
     const saved = await fileDb.save(savingObj);
     await fileDb.save({
       ...saved,
-      testValue: "value after updating"
+      testValue: "value after updating",
     });
 
     assert.equal(
-      await fileDb.get(saved._id)
-        .then(({ testValue }) => testValue),
+      await fileDb.get(saved._id).then(({ testValue }) => testValue),
       "value after updating"
     );
   });
 
   it("getIds() returns a Promise that resolves string array", async () => {
-    assert(
-      (await fileDb.getIds())
-        .every(key => typeof key === "string")
-    )
+    assert((await fileDb.getIds()).every((key) => typeof key === "string"));
   });
 
   it("remove()", async () => {
@@ -74,9 +67,6 @@ describe("FileDb", () => {
     const allObj = await fileDb.get();
     const key = Object.keys(allObj)[0];
     await fileDb.remove(key);
-    assert.equal(
-      key in await fileDb.get(),
-      false
-    );
+    assert.equal(key in (await fileDb.get()), false);
   });
 });
